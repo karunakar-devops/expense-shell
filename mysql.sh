1d69825053a6e6daeca332799bcc1c1ca18128e5
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-24 16:00:55 +0530

    Mysql server

---
+++ mysql.sh
@@ -0,0 +1,32 @@
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root access"
+    #exit 1
+else
+    echo "You are Super User"
+fi
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+    echo "$2 is...... Failure"
+    #exit 1
+else
+    echo "$2 is..... Suceess"
+fi
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld 
+VALIDATE $? "Enabling mysql server"
+
+systemctl start mysqld
+VALIDATE $? "startting mysql server"
+
+mysql_secure_Installation --set -root -pass ExpenseApp@1
+VALIDATE $? "setting up root password"
