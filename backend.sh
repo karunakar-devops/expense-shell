6438847e2bc9b30f895d003879c96b0f56ec466d
karunakr-devops <karunakar.etikala@gmail.com>
2024-04-27 12:58:03 +0530

    expense project

--- backend.sh
+++ backend.sh
@@ -68,7 +68,7 @@ VALIDATE $? "Unzipping files"
 npm install &>>$LOGFILE
 VALIDATE $? "Installing nodejs dependencies"
 
-cp C:\devops\daws-78s\repos-practice\expense-shell\backend.service /etc/systemd/sytem/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 VALIDATE $? "copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
